85175947042ebe4ab9462a56ce914e2fae3c6431
aalejandrozuleta <jhoitan266@gmail.com>
2023-06-28 17:29:39 -0500

    full

---
+++ 1/1.js
@@ -0,0 +1,54 @@
+arregloUno = [ ["a", "z", "t"], [789, 887, 451], [true, false, 0]]
+console.log(arregloUno[0][1]);
+console.log(arregloUno[1][0]);
+console.log(arregloUno[2][2]);
+console.log(arregloUno[2][1]);
+arregloDos =  [ [ ["%", 7, true, "#"] ], [ [false, 0, 99, "?"] ], [ [44, 55, 66, 77] ] ]
+console.log(arregloDos[1][0][3]);
+console.log(arregloDos[2][0][2]);
+console.log(arregloDos[0][0][2]);
+console.log(arregloDos[0][0][0]);
+console.log(arregloDos[0][0][2]);
+console.log(arregloDos[2][0][0]);
+console.log(arregloDos[1][0][3]);
+arregloTres= [ [ [0], [2, 77] ], [ ["&", true, "!!"], [[88]] ], [ [ [ [1] ] ] ], "¿" ]
+console.log(arregloTres[1][0][1]);
+console.log(arregloTres[2][0][0][0][0]);
+console.log(arregloTres[0][1][1]);
+console.log(arregloTres[1][1][0][0]);
+console.log(arregloTres[3]);
+arregloCuatro= [ [50, 60, 70], ["q", "%", "$"], ["?", ">", "<"], [1 , -8, -2] ]
+console.log(arregloCuatro[1]);
+arregloCuatro[1][1] = "?";
+console.log(arregloCuatro[1]);
+console.log(arregloCuatro[3]);
+arregloCuatro[3][1] = 9;
+console.log(arregloCuatro[3]);
+console.log(arregloCuatro[0]);
+arregloCuatro[0][0] = "¡";
+console.log(arregloCuatro[0]);
+arregloCuatro[1].splice(2);
+console.log(arregloCuatro);
+arregloCuatro[0].splice(2);
+console.log(arregloCuatro);
+arregloCuatro[3].splice(2);
+console.log(arregloCuatro);
+arregloTres= [ [ [0], [2, 77] ], [ ["&", true, "!!"], [[88]] ], [ [ [ [1] ] ] ], "¿" ]
+console.log(arregloTres[1][0][1]);
+console.log(arregloTres[2][0][0][0][0]);
+console.log(arregloTres[0][1][1]);
+console.log(arregloTres[1][1][0][0]);
+console.log(arregloTres[3]);
+arregloCuatro= [ [50, 60, 70], ["q", "%", "$"], ["?", ">", "<"], [1 , -8, -2] ]
+console.log(arregloCuatro[1]);arregloCuatro[1][1] = "?";console.log(arregloCuatro[1]);console.log(arregloCuatro[3]);
+arregloCuatro[3][1] = 9;
+console.log(arregloCuatro[3]);
+console.log(arregloCuatro[0]);
+arregloCuatro[0][0] = "¡";
+console.log(arregloCuatro[0]);
+arregloCuatro[1].splice(2);
+console.log(arregloCuatro);
+arregloCuatro[0].splice(2);
+console.log(arregloCuatro);
+arregloCuatro[3].splice(2);
+console.log(arregloCuatro);
